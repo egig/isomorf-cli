1763a9396c52a83552dc1d64f13a1a1f2976c138
Egi Gundari <egi.gundari@qraved.com>
2017-06-23 09:10:04 +0700

    init

---
+++ isomorf-cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const program  = require('commander');
+
+program
+	.version('0.1.0')
+	.parse(process.argv);
+
+console.log('Hello this is isomorph cli is no really useful now');
